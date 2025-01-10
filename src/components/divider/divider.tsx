bb999e5fde7a6239a04b93db092f3402eecb1db0
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-10 17:25:50 -0300

    feat: add component add dividier

--- src/components/divider/divider.tsx
+++ src/components/divider/divider.tsx
@@ -2,15 +2,9 @@ interface DividerProps {
   size?: 'small' | 'medium' | 'large';
 }
 
-const marginSizes = {
-  small: '4px',
-  medium: '8px',
-  large: '16px',
-};
-
 export function Divider({ size = 'small' }: DividerProps) {
   const styleDivider = {
-    margin: marginSizes[size],
+    margin: size === 'small' ? '4px' : size === 'medium' ? '8px' : '16px',
   };
 
   return <div style={styleDivider} />;
