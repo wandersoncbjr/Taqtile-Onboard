146c6677d875d7e43d693caaf1e88c171095051a
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-10 17:26:51 -0300

    feat: add component login

---
+++ src/components/pages/login/login.tsx
@@ -0,0 +1,21 @@
+import { Button } from '../../button/button';
+import { ContainerCard } from '../../container-card/container-card';
+import { Divider } from '../../divider/divider';
+import { Input } from '../../input/input';
+import './Login.css';
+
+export function Login() {
+  return (
+    <div className='container-login'>
+      <ContainerCard>
+        <h1>Bem vindo(a) à Taqtile!</h1>
+        <Divider size='large' />
+        <Input expand type='text' label='E-mail' />
+        <Divider size='small' />
+        <Input expand label='Senha' type='password' error='Senha Incorreta' />
+        <Divider size='large' />
+        <Button expand>Entrar</Button>
+      </ContainerCard>
+    </div>
+  );
+}
