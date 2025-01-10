0dfba53878a749094ab0e4ff9806cf018a8169a5
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-10 17:27:29 -0300

    feat: add theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    PrimaryXDark: '#000000',
+    FeedbackError: '#ff0000',
+    NeutralLight: '#7fffd4',
+    Neutral: '#ffffff',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  },
+};
