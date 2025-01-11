394b0efe8e5f436219506e3e9b827fecc7e4cacc
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-11 18:12:27 -0300

    feat: extend ButtonProps to accept additional button attributes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,19 +1,19 @@
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import './button.css';
 import { theme } from '../../theme/theme';
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   expand?: boolean;
 }
 
-export function Button({ children, expand }: ButtonProps) {
+export function Button({ children, expand, ...rest }: ButtonProps) {
   const buttonStyle = {
     width: expand ? '100%' : '200px',
     backgroundColor: theme.colors.NeutralLight,
   };
   return (
-    <button style={buttonStyle} className='button-element'>
+    <button {...rest} style={buttonStyle} className='button-element'>
       {children}
     </button>
   );
