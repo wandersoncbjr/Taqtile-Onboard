73eaf8339fcb50383b4cc58d14ea666cf118bb65
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-11 22:19:29 -0300

    feat: add regex and message validation

---
+++ src/validation-forms/validation-messages.ts
@@ -0,0 +1,11 @@
+export const errorMessages = {
+  email: {
+    required: 'E-mail é obrigatório',
+    pattern: 'Por favor, insira um e-mail válido',
+  },
+  senha: {
+    required: 'Senha é obrigatória',
+    minLength: 'A senha deve ter pelo menos 7 caracteres',
+    pattern: 'Senha deve conter pelo menos uma letra e um número',
+  },
+};
---
+++ src/validation-forms/validation-regex.ts
@@ -0,0 +1,4 @@
+export const validationRegex = {
+  email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+  password: /^(?=.*[a-zA-Z])(?=.*\d).+$/,
+};
