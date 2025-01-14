69435d9d61a8aaa203bb80083e0d31d26832b865
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-14 00:46:44 -0300

    fix: password

--- src/validation-forms/validation-messages.ts
+++ src/validation-forms/validation-messages.ts
@@ -3,7 +3,7 @@ export const errorMessages = {
     required: 'E-mail é obrigatório',
     pattern: 'Por favor, insira um e-mail válido',
   },
-  senha: {
+  password: {
     required: 'Senha é obrigatória',
     minLength: 'A senha deve ter pelo menos 7 caracteres',
     pattern: 'Senha deve conter pelo menos uma letra e um número',
