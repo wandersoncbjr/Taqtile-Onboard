53b0a888781c4bd0f4772561c64c40add39e7fac
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-14 00:45:45 -0300

    feat: refactor login component to use custom hook for form handling and validation

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -2,31 +2,20 @@ import { Button } from '../../button/button';
 import { Card } from '../../card/card';
 import { Divider } from '../../divider/divider';
 import { Input } from '../../input/input';
-import { useForm } from 'react-hook-form';
 import './Login.css';
 import { validationRegex } from '../../../validation-forms/validation-regex';
 import { errorMessages } from '../../../validation-forms/validation-messages';
-
-interface LoginFormData {
-  email: string;
-  senha: string;
-}
+import { useLoginForm } from './hooks/useLoginForm';
 
 export function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginFormData>();
-
-  const onSubmit = (data: LoginFormData) => {};
+  const { register, error, handleSubmit, errors, handleForm, loading } = useLoginForm();
 
   return (
     <div className='container-login'>
       <Card>
         <h1>Bem-vindo(a) à Instaq!</h1>
         <Divider size='large' />
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(handleForm)}>
           <Input
             expand
             type='text'
@@ -47,21 +36,21 @@ export function Login() {
             type='password'
             label='Senha'
             placeholder='Digite sua senha'
-            {...register('senha', {
-              required: errorMessages.senha.required,
+            {...register('password', {
+              required: errorMessages.password.required,
               minLength: {
                 value: 7,
-                message: errorMessages.senha.minLength,
+                message: errorMessages.password.minLength,
               },
               pattern: {
                 value: validationRegex.password,
-                message: errorMessages.senha.pattern,
+                message: errorMessages.password.pattern,
               },
             })}
-            error={errors.senha?.message}
+            error={errors.password?.message || error?.message}
           />
           <Divider size='large' />
-          <Button expand type='submit'>
+          <Button expand type='submit' disabled={loading}>
             Entrar
           </Button>
         </form>
