3679f8de5aa1234139f67f7f1268aadb4cd6e3f1
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-15 14:49:12 -0300

    feat: add LoadingScreen component and integrate it into Home page

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,7 +1,10 @@
+import { LoadingScreen } from '@components/loading-screen/loading-screen';
+
 export function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <LoadingScreen />
     </div>
   );
 }
