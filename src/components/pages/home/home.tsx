615654ce9bd9589101086dc5fb5c28cbe89b9281
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-15 15:43:41 -0300

    feat: add user table to Home page with styling

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,10 +1,42 @@
-import { LoadingScreen } from '@components/loading-screen/loading-screen';
+import { Card } from '@components/card/card';
+import './home.css';
 
 export function Home() {
+  const users = [
+    { name: 'Wanderson', email: 'wanderson@example.com' },
+    { name: 'Lucas Silva', email: 'lucas.silva@example.com' },
+    { name: 'Maria Oliveira', email: 'maria.oliveira@example.com' },
+    { name: 'João Souza', email: 'joao.souza@example.com' },
+    { name: 'Lucas Silva', email: 'lucas.silva@example.com' },
+    { name: 'Maria Oliveira', email: 'maria.oliveira@example.com' },
+    { name: 'João Souza', email: 'joao.souza@example.com' },
+    { name: 'Lucas Silva', email: 'lucas.silva@example.com' },
+    { name: 'Maria Oliveira', email: 'maria.oliveira@example.com' },
+    { name: 'João Souza', email: 'joao.souza@example.com' },
+    { name: 'Lucas Silva', email: 'lucas.silva@example.com' },
+    { name: 'Maria Oliveira', email: 'maria.oliveira@example.com' },
+    { name: 'João Souza', email: 'joao.souza@example.com' },
+  ];
+
   return (
-    <div>
+    <Card>
       <h1>Home</h1>
-      <LoadingScreen />
-    </div>
+      <table className='user-table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Card>
   );
 }
