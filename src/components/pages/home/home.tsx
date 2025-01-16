64926d14f051bc470c0b43689156764e1e24004d
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-15 22:16:17 -0300

    refactor: replace users table with ListUser component and modify styles on home page

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,8 +1,8 @@
-import { Card } from '@components/card/card';
+import { ListUser } from '@components/list-user/list-user';
 import './home.css';
 
 export function Home() {
-  const users = [
+  const userList = [
     { name: 'Wanderson', email: 'wanderson@example.com' },
     { name: 'Lucas Silva', email: 'lucas.silva@example.com' },
     { name: 'Maria Oliveira', email: 'maria.oliveira@example.com' },
@@ -19,24 +19,9 @@ export function Home() {
   ];
 
   return (
-    <Card>
+    <div className='home-container'>
       <h1>Home</h1>
-      <table className='user-table'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={index}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </Card>
+      <ListUser userList={userList} />
+    </div>
   );
 }
