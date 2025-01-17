ce9aa17b5e2dfd93a9d9f955dcdc4f34324678d1
wandersoncbjr <wandersoncbjr2@gmail.com>
2025-01-17 00:53:45 -0300

    feat: add validation messages and regular expressions for name, phone, title and date of birth

--- src/validation-forms/validation-messages.ts
+++ src/validation-forms/validation-messages.ts
@@ -8,4 +8,21 @@ export const errorMessages = {
     minLength: 'A senha deve ter pelo menos 7 caracteres',
     pattern: 'Senha deve conter pelo menos uma letra e um número',
   },
+  name: {
+    required: 'O Nome Completo é obrigatório',
+    pattern: 'O Nome Completo deve conter pelo menos um nome e um sobrenome',
+  },
+  phone: {
+    required: 'O número de Telefone é obrigatório',
+    pattern: 'O número de Telefone deve conter apenas números e ter entre 10 e 11 dígitos',
+  },
+  role: {
+    required: 'O Cargo é obrigatório',
+  },
+
+  birthDate: {
+    required: 'A Data de Nascimento é obrigatória',
+    futureDate: 'A data de nascimento não pode estar no futuro.',
+    minDate: 'A data de nascimento deve ser posterior a 01/01/1900.',
+  },
 };
